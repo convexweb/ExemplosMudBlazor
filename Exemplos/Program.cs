5406da99fb15e3a508f39f4640e4a6f97aee5d4e
convexweb <fabiano.convexweb@gmail.com>
2024-05-06 20:14:20 -0300

    Add project files.

---
+++ Exemplos/Program.cs
@@ -0,0 +1,43 @@
+using Exemplos.Client.Pages;
+using Exemplos.Components;
+using MudBlazor.Services;
+
+namespace Exemplos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            builder.Services.AddMudServices();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+            app.Run();
+        }
+    }
+}
